44538dfaf87a6527a451728c04524012ecdc7e89
Hamza Mahjabi <hamzamahjabi@gmail.com>
2024-04-17 07:04:54 -0700

    reas

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
-import os
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-
+"""Esta es la clase de amenidades"""
+import models
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+# from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """Represents an amenity data set."""
+    """Esta es la clase de Amenity
+    Atributos:
+        name: nombre de entrada
+    """
     __tablename__ = 'amenities'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    name = Column(String(128), nullable=False)
+    # place_amenities = relationship('Place', secondary='place_amenity')
--- models/state.py
+++ models/state.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import os
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-
 from models.base_model import BaseModel, Base
+import models
 from models.city import City
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship(
-            'City',
-            cascade='all, delete, delete-orphan',
-            backref='state'
-        )
+    if models.is_type == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='delete')
     else:
+        name = ""
+
+    if models.is_type != 'db':
         @property
         def cities(self):
-            """Returns the cities in this State"""
-            from models import storage
-            cities_in_state = []
-            for value in storage.all(City).values():
-                if value.state_id == self.id:
-                    cities_in_state.append(value)
-            return cities_in_state
+            cities_list = []
+            all_cities = models.storage.all(City).values()
+            for city in all_cities:
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
